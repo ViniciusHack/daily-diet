10853ab0c33ef66d54c0b75d7b8aa0bcda601ab5
vinicius <viniciusvwh1441@gmail.com>
2023-05-28 23:45:08 -0300

    tests: app tests structure

---
+++ test/meals.spec.ts
@@ -0,0 +1,43 @@
+import { describe, it } from 'vitest'
+
+describe('Meals routes', () => {
+  describe('Users', () => {
+    it('should be able to create a new user', () => {})
+
+    it('should not be able to create a user that already exists', () => {})
+  })
+
+  it('should be able to create a new meal', () => {})
+
+  it('should be able to edit a meal', () => {})
+
+  it('should not be able to edit a meal from another user', () => {})
+
+  it('should be able to delete a meal', () => {})
+
+  it('should not be able to delete a meal from another user', () => {})
+
+  it('should be able to list all meals from a user', () => {})
+
+  it('should not be able to list all meals from another user', () => {})
+
+  it('should be able to get a specific meal', () => {})
+
+  it('should not be able to get a specific meal from another user', () => {})
+
+  it('should be able to get the metrics from the user', () => {})
+
+  it('should not be able to get the metrics from another user', () => {})
+  //   - Deve ser possível registrar uma refeição feita, com as seguintes informações:
+  //     *As refeições devem ser relacionadas a um usuário.*
+  //     - Nome
+  //     - Descrição
+  //     - Data e Hora
+  //     - Está dentro ou não da dieta
+  // - Deve ser possível recuperar as métricas de um usuário
+  //     - Quantidade total de refeições registradas
+  //     - Quantidade total de refeições dentro da dieta
+  //     - Quantidade total de refeições fora da dieta
+  //     - Melhor sequência por dia de refeições dentro da dieta
+  // - O usuário só pode visualizar, editar e apagar as refeições o qual ele criou
+})
